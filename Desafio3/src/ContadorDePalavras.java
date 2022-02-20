f2b6a0275232991d32f726346b6bdd4c97116603
André Luiz <andlu-luz@hotmail.com>
2022-02-20 12:10:24 -0300

    Desafio Capgemini

---
+++ Desafio3/src/ContadorDePalavras.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class ContadorDePalavras {
+    public ContadorDePalavras(String palavra) {
+        HashMap<String, Integer> map = new HashMap<String, Integer>();
+        for (int i = 0; i < palavra.length(); i++) {
+            String key = palavra.substring(i, i + 1);
+            if (map.containsKey(key)) {
+                map.put(key, map.get(key) + 1);
+            } else {
+                map.put(key, 1);
+            }
+        }
+        System.out.println(map);
+    }
+    public static <ContadorParesAnagramas> void main(String[] args) {
+        System.out.println("Digite uma palabra: ");
+        Scanner sc = new Scanner(System.in);
+        String palavra = sc.nextLine();
+        ContadorParesAnagramas contador = (ContadorParesAnagramas) new ContadorDePalavras(palavra);
+
+    }
+}
